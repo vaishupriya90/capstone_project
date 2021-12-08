62187a291c5a57d59c00d2d02f6c58fe2d48bb44
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2021-12-08 16:31:14 -0600

    display - Refactored the code: display single painting item - e2e pass - VC

--- web/src/components/Painting.jsx
+++ web/src/components/Painting.jsx
@@ -7,17 +7,13 @@ import axios from 'axios';
 
 const Painting = () => {
   const [error, setError] = useState(null);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  // const [imageUrl, setImageUrl] = useState('');
+  const [painting, setPainting] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get('https://localhost:5001/api/painting');
-        setName(data.name);
-        setDescription(data.description);
-        // setImageUrl(data.imageUrl);
+        setPainting(data);
       } catch (err) {
         setError(err);
       }
@@ -35,11 +31,11 @@ const Painting = () => {
     <div>
       <Container className="mt-5">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1552353617-3bfd679b3bdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Ym9hdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" />
+          <Card.Img variant="top" src={painting.image} />
           <Card.Body>
-            <Card.Title>{name}</Card.Title>
+            <Card.Title>{painting.name}</Card.Title>
             <Card.Text>
-              {description}
+              {painting.description}
             </Card.Text>
             <Button variant="dark">Add to cart</Button>
           </Card.Body>
