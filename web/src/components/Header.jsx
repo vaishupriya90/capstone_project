d890b3390738e0af1696502136566bd9bbadca30
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2021-12-22 14:42:40 -0600

    add - update number of items in cart as items are added to cart - VC

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,26 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   Badge, Container, FormControl, Nav, Navbar, Button,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 
-function Header() {
-  return (
-    <Navbar className="navbar" bg="dark" variant="dark" style={{ height: 80 }}>
-      <Container>
-        <Navbar.Brand>
-          <Link to="/">The Art Shop</Link>
-        </Navbar.Brand>
-        <Nav style={{ color: 'white' }}>
-          <Nav.Link>
-            <Link to="/paintings">Paintings</Link>
-          </Nav.Link>
-        </Nav>
-        <Navbar.Text className="search">
-          <FormControl className="m-auto" style={{ width: 500 }} placeholder="Search for a product" />
-        </Navbar.Text>
-        {/* <Dropdown align="end">
+export const Header = ({ cartItems }) => (
+  <Navbar className="navbar" bg="dark" variant="dark" style={{ height: 80 }}>
+    <Container>
+      <Navbar.Brand>
+        <Link to="/">The Art Shop</Link>
+      </Navbar.Brand>
+      <Nav style={{ color: 'white' }}>
+        <Nav.Link>
+          <Link to="/paintings">Paintings</Link>
+        </Nav.Link>
+      </Nav>
+      <Navbar.Text className="search">
+        <FormControl className="m-auto" style={{ width: 500 }} placeholder="Search for a product" />
+      </Navbar.Text>
+      {/* <Dropdown align="end">
           <Dropdown.Toggle variant="success">
             <Link to="/cart"><FaShoppingCart color="white" fontSize="25px" /></Link>
             <Badge bg="success">{0}</Badge>
@@ -32,17 +33,20 @@ function Header() {
 
         </Dropdown> */}
 
-        <Link to="/cart">
-          <Button align="end" variant="success">
-            <FaShoppingCart color="white" fontSize="25px" />
-            {' '}
-            <Badge bg="success">{0}</Badge>
-          </Button>
-        </Link>
-      </Container>
+      <Link to="/cart">
+        <Button align="end" variant="success">
+          <FaShoppingCart color="white" fontSize="25px" />
+          {' '}
+          <Badge bg="success">{cartItems.length}</Badge>
+        </Button>
+      </Link>
+    </Container>
 
-    </Navbar>
-  );
-}
+  </Navbar>
+);
 
-export default Header;
+const mapStateToProps = (state) => ({
+  cartItems: state.cartItemList.cartItems,
+});
+
+export default connect(mapStateToProps)(Header);
