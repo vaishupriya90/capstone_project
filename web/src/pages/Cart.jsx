d7264cdd39c14e6767a244d9107db3baed220f80
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-01-02 01:27:32 -0600

    display - display list of cart items - VC

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -1,8 +1,62 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
-const Cart = () => (
-  <div>
-    cart
+
+import { connect } from 'react-redux';
+import {
+  Button, ListGroup, Row, Col, Image,
+} from 'react-bootstrap';
+import { AiFillDelete } from 'react-icons/ai';
+
+// eslint-disable-next-line react/prop-types
+const Cart = ({ cartItems }) => (
+  <div className="home">
+    <div className="productContainer">
+      <ListGroup>
+        {cartItems.map((prod) => (
+          <ListGroup.Item key={prod.id}>
+            <Row>
+              <Col md={2}>
+                <Image src={prod.painting.image} alt={prod.painting.name} fluid rounded />
+              </Col>
+              <Col md={2}>
+                <span>{prod.painting.name}</span>
+              </Col>
+              <Col md={2}>$100</Col>
+              <Col md={2}>
+                {/* <Form.Control
+                    as="select"
+                    value={prod.qty}
+                    onChange={(e) =>
+                      dispatch({
+                        type: "CHANGE_CART_QTY",
+                        payload: {
+                          id: prod.id,
+                          qty: e.target.value,
+                        },
+                      })
+                    }
+                  > */}
+
+                {/* </Form.Control> */}
+              </Col>
+              <Col md={2}>
+                <Button
+                  type="button"
+                  variant="light"
+                >
+                  <AiFillDelete fontSize="20px" />
+                </Button>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
   </div>
 );
 
-export default Cart;
+const mapStateToProps = (state) => ({
+  cartItems: state.cartItemList.cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
