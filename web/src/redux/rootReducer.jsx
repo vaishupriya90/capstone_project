1530bbf5409e9ae1ac9991a1a4bbe8453225546e
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-01-03 16:23:25 -0600

    add - included filter reducer in the root reducer - VC

--- web/src/redux/rootReducer.jsx
+++ web/src/redux/rootReducer.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cartReducer from './cart/cart-reducer';
 import paintingReducer from './painting/painting-reducer';
+import filterReducer from './Filters/filter-reducer';
 
 const rootReducer = combineReducers({
   paintingList: paintingReducer,
   cartItemList: cartReducer,
+  filters: filterReducer,
 });
 
 export default rootReducer;
