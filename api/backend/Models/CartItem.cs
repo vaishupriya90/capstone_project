e0fcf9a1537ccc44d40d921abf566e660c8c0854
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-01-05 12:51:56 -0600

    add - changed the Id type to be int - VC

--- api/backend/Models/CartItem.cs
+++ api/backend/Models/CartItem.cs
@@ -6,8 +6,8 @@ namespace backend.Models
     public class CartItem
     {
         [Key]
-        public long Id { get; set; }
-        public long PaintingId { get; set; }
+        public int Id { get; set; }
+        public int PaintingId { get; set; }
         public int Quantity { get; set; }
         public virtual Painting Painting { get; set; }
     }
--- api/backend/Models/Painting.cs
+++ api/backend/Models/Painting.cs
@@ -6,18 +6,21 @@ namespace backend.Models
     public class Painting
     {
         [Key]
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
 
+        public double? Price  {get; set;} 
+
         public virtual ICollection<CartItem> CartItems { get; set; }
 
-        public Painting(string name, string description, string image)
+        public Painting(string name, string description, string image, double price)
         {
             Name = name;
             Description = description;
             Image = image;
+            Price = price;
         }
 
         public Painting(string name)
