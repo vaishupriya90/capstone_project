f85ef4a8048d47f8e4b48f6cb4588db86eaeb6e5
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-01-17 01:09:50 -0600

    minor - added shop button in the home page which links to paintings page - VC

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { HiShoppingBag } from 'react-icons/hi';
 
 const Home = () => (
   <div className="homePage">
     Home
+    <div className="homeButton">
+      <h1 style={{ color: 'white' }}>Great Products, Great Price!</h1>
+      <Link to="/paintings">
+        <Button variant="light">
+          <HiShoppingBag />
+          <span>Shop Now</span>
+        </Button>
+      </Link>
+
+    </div>
+
   </div>
 );
 
