12d0232b673bb76982f0bd8632e07833c8cdf8a2
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-01-30 20:24:18 -0600

    navbar - Added react-bootstrap Stack component in Navbar - VC

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -3,90 +3,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useAuth0 } from '@auth0/auth0-react';
 import {
-  Badge, Container, FormControl, Nav, Navbar, Dropdown,
-  Button,
+  Badge, Container, FormControl, Nav, Navbar, Dropdown, Stack,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart, FaPaintBrush } from 'react-icons/fa';
-import { AiFillDelete } from 'react-icons/ai';
+// import { AiFillDelete } from 'react-icons/ai';
 import { search } from '../redux/Filters/filter-actions';
 import { removeFromCart } from '../redux/cart/cartSlice';
 import getCartItems from '../redux/cart/selectors';
 import UserLogin from './authentication/UserLogin';
+import NavCartDropDown from './NavCartDropDown';
 
 export const Header = ({ cartItems, searchText, removeItemFromCart }) => {
   const { isAuthenticated, user } = useAuth0();
   return (
     <Navbar className="navbar header" variant="dark" style={{ height: 80 }}>
-      <Container>
-        <Navbar.Brand>
-          <FaPaintBrush fontSize="60px" color="orange" />
-          <Link to="/" style={{ fontFamily: 'cursive', fontSize: '25px' }}>The Art Shop</Link>
-        </Navbar.Brand>
-        <Nav style={{ color: 'white' }}>
-          <Link to="/paintings">Paintings</Link>
-        </Nav>
-        <Navbar.Text className="search">
-          <FormControl className="m-auto" style={{ width: 500 }} placeholder="Search for a product" onChange={(e) => searchText(e.target.value)} />
-        </Navbar.Text>
-        <Dropdown className="row justify-content-end">
-          <Dropdown.Toggle variant="rgb(44, 114, 85">
-            <FaShoppingCart color="white" fontSize="30px" />
-            <Badge bg="rgb(44, 114, 85">{cartItems.length}</Badge>
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu style={{ minWidth: 350 }}>
-            {cartItems.length > 0 ? (
-              <>
-                {cartItems.map((prod) => (
-                  <>
-                    <span className="cartitem" key={prod.painting.id}>
-                      <img
-                        src={prod.painting.image}
-                        className="cartItemImg"
-                        alt={prod.painting.name}
-                      />
-                      <div className="cartItemDetail">
-                        <span>{prod.painting.name}</span>
-                        <span>
-                          $
-                          {' '}
-                          {prod.painting.price}
-                        </span>
-                      </div>
-                      <AiFillDelete
-                        fontSize="20px"
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => removeItemFromCart(prod.painting)}
-                      />
-                    </span>
-                    <div className="dropdown-divider" />
-
-                  </>
-                ))}
-                <Link to="/cart">
-                  <Button style={{ width: '95%', margin: '0 10px' }}>
-                    Go To Cart
-                  </Button>
-                </Link>
-              </>
-            ) : (
-              <span style={{ padding: 10 }}>Cart is Empty!</span>
-            )}
-          </Dropdown.Menu>
-
-        </Dropdown>
-        <UserLogin />
-        {isAuthenticated ? (
-          <span style={{ color: 'white' }}>
-            Welcome
-            {' '}
-            {user.email}
-            !
-          </span>
-        ) : ''}
+      <Container fluid>
+        <Stack direction="horizontal">
+          <Navbar.Brand>
+            <FaPaintBrush fontSize="60px" color="orange" />
+            <Link to="/" style={{ fontFamily: 'cursive', fontSize: '25px' }}>The Art Shop</Link>
+          </Navbar.Brand>
+          <Nav style={{ color: 'white' }}>
+            <Link to="/paintings">Paintings</Link>
+          </Nav>
+        </Stack>
+        <Stack direction="horizontal" gap="5">
+          <Navbar.Text className="search">
+            <FormControl className="m-auto" style={{ width: 500 }} placeholder="Search for a product" onChange={(e) => searchText(e.target.value)} />
+          </Navbar.Text>
+          <Dropdown className="row justify-content-end">
+            <Dropdown.Toggle variant="rgb(44, 114, 85">
+              <FaShoppingCart color="white" fontSize="30px" />
+              <Badge bg="rgb(44, 114, 85">{cartItems.length}</Badge>
+            </Dropdown.Toggle>
+            <NavCartDropDown cartItems={cartItems} removeItemFromCart={removeItemFromCart} />
+          </Dropdown>
+          <UserLogin />
+          {isAuthenticated ? (
+            <span style={{ color: 'white' }}>
+              Welcome
+              {' '}
+              {user.email}
+              !
+            </span>
+          ) : ''}
+        </Stack>
       </Container>
-
     </Navbar>
   );
 };
---
+++ web/src/components/NavCartDropDown.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Dropdown } from 'react-bootstrap';
+import { AiFillDelete } from 'react-icons/ai';
+
+export default function NavCartDropDown({ cartItems, removeItemFromCart }) {
+  return (
+    <Dropdown.Menu>
+      {cartItems.length > 0 ? (
+        <>
+          {cartItems.map((prod) => (
+            <>
+              <span className="cartitem" key={prod.painting.id}>
+                <img
+                  src={prod.painting.image}
+                  className="cartItemImg"
+                  alt={prod.painting.name}
+                />
+                <div className="cartItemDetail">
+                  <span>{prod.painting.name}</span>
+                  <span>
+                    $
+                    {' '}
+                    {prod.painting.price}
+                  </span>
+                </div>
+                <AiFillDelete
+                  fontSize="20px"
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => removeItemFromCart(prod.painting)}
+                />
+              </span>
+              <div className="dropdown-divider" />
+            </>
+          ))}
+          <Link to="/cart">
+            <Button style={{ width: '95%', margin: '0 10px' }}>
+              Go To Cart
+            </Button>
+          </Link>
+        </>
+      ) : (
+        <span style={{ padding: 10 }}>Cart is Empty!</span>
+      )}
+    </Dropdown.Menu>
+
+  );
+}
