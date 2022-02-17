d9e7d9da8020b3c204deea53aad75cdd4ecdf311
Vaishnava Priya Chellaiya <vaishnavapriya.chellaiya@wwt.com>
2022-02-16 19:12:42 -0600

    18 - redirects to login page if the user tries to checkout without logging in - VC

--- web/src/components/cart/CheckoutButton.jsx
+++ web/src/components/cart/CheckoutButton.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { Button } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const CheckoutButton = ({ cartItems }) => {
+  const {
+    loginWithRedirect, isAuthenticated,
+  } = useAuth0();
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+    navigate('/cart');
+  };
   if (cartItems.length === 0) {
     return (
       <span>
@@ -19,15 +32,16 @@ const CheckoutButton = ({ cartItems }) => {
     );
   }
   return (
-    <Link to="/cart">
-      <Button
-        type="button"
-        variant="success"
-        size="md"
-      >
-        Proceed To Checkout
-      </Button>
-    </Link>
+
+    <Button
+      onClick={handleClick}
+      type="button"
+      variant="success"
+      size="md"
+    >
+      Proceed To Checkout
+    </Button>
+
   );
 };
 
